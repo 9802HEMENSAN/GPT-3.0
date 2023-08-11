a40bd14d017bf759803419738e39de7ce850d507
9802HEMENSAN <hmahilange9802@gmail.com>
2023-08-11 23:52:09 +0530

    multiselect optiion

---
+++ company-process/material-multiselect/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MultipleSelectChip from './components/MultiSelect';
+
+function App() {
+  return (
+    <div className="App">
+       <MultipleSelectChip/>
+    </div>
+  );
+}
+
+export default App;
