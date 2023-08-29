70ab28f4972cdee04a221454d27694519bbe96ed
9802HEMENSAN <hmahilange9802@gmail.com>
2023-08-29 22:30:40 +0530

    react

---
+++ company-process/locofy-react-project/src/pages/GatewayInvestPeHome.tsx
@@ -0,0 +1,421 @@
+import { FunctionComponent } from "react";
+import styles from "./GatewayInvestPeHome.module.css";
+const GatewayInvestPeHome: FunctionComponent = () => {
+  return (
+    <div className={styles.gatewayInvestpeHome}>
+      <img
+        className={styles.gatewayInvestpeHomeChild}
+        alt=""
+        src="/group-1.svg"
+      />
+      <div className={styles.rectangleParent}>
+        <div className={styles.frameChild} />
+        <div className={styles.frameChild} />
+        <div className={styles.image1} />
+        <div className={styles.launchFinancialServices}>
+          Launch financial services in a matter of days. Take your pick from our
+          flexible APIs, SDKs, or ready-to-use screens.
+        </div>
+        <div className={styles.frameInner} />
+        <div className={styles.rectangleDiv} />
+        <div className={styles.contactUs}>Contact Us</div>
+        <div className={styles.ellipseDiv} />
+        <div className={styles.seeOurProducts}>{`See our products `}</div>
+        <div className={styles.chevronDownSvgrepoCom11} />
+        <img className={styles.vectorIcon} alt="" src="/vector.svg" />
+        <div className={styles.elevateYourInvestmentContainer}>
+          <span>{`Elevate Your Investment Strategy with Our `}</span>
+          <span className={styles.comprehensiveApis}>Comprehensive APIs</span>
+        </div>
+        <b className={styles.investmentStackForContainer}>
+          <span>INVESTMENT STACK</span>
+          <span className={styles.forIndia}> for India</span>
+        </b>
+        <div className={styles.stocks}>Stocks</div>
+        <div className={styles.mutualFunds}>Mutual Funds</div>
+        <div className={styles.frameChild1} />
+        <div className={styles.digitalGold}>Digital Gold</div>
+        <img
+          className={styles.ebbce30259c4b43Ac6135c515dIcon}
+          alt=""
+          src="/1ebbce30259c4b43ac6135c515dac52b-1@2x.png"
+        />
+        <img
+          className={styles.ebbce30259c4b43Ac6135c515dIcon1}
+          alt=""
+          src="/1ebbce30259c4b43ac6135c515dac52b-3@2x.png"
+        />
+        <img
+          className={styles.ebbce30259c4b43Ac6135c515dIcon2}
+          alt=""
+          src="/1ebbce30259c4b43ac6135c515dac52b-2@2x.png"
+        />
+        <div className={styles.index1} />
+        <div className={styles.index2} />
+      </div>
+      <div className={styles.gatewayInvestpeHomeItem} />
+      <div className={styles.gatewayInvestpeHomeInner} />
+      <img className={styles.vectorIcon1} alt="" src="/vector1.svg" />
+      <img
+        className={styles.investpe01121}
+        alt=""
+        src="/investpe-0112-1@2x.png"
+      />
+      <div className={styles.gatewayInvestpeHomeChild1} />
+      <div className={styles.gateway}>Gateway</div>
+      <div className={styles.gatewayInvestpeHomeChild2} />
+      <div className={styles.aboutus}>AboutUs</div>
+      <div className={styles.products}>Products</div>
+      <img
+        className={styles.screenshot20230820At125}
+        alt=""
+        src="/screenshot-20230820-at-125-1@2x.png"
+      />
+      <div className={styles.features}>FEATURES</div>
+      <b className={styles.launchTheMost}>
+        Launch the most Engaging Invest-Tech Solutions
+      </b>
+      <div className={styles.solution}>SOLUTION</div>
+      <b className={styles.stayAheadWithContainer}>
+        <p className={styles.stayAheadWith}>{`Stay ahead with our `}</p>
+        <p className={styles.stayAheadWith}>Wealth-tech as a service</p>
+      </b>
+      <div className={styles.weOfferA}>
+        We offer a suite of B2B Invest-tech solutions, that are cost-effective
+        way to offer an engaging investment experience to your customers, with a
+        faster time to market
+      </div>
+      <div className={styles.gatewayInvestpeHomeChild3} />
+      <div className={styles.gatewayInvestpeHomeChild4} />
+      <div className={styles.gatewayInvestpeHomeChild5} />
+      <div className={styles.gatewayInvestpeHomeChild6} />
+      <div className={styles.gatewayInvestpeHomeChild7} />
+      <div className={styles.gatewayInvestpeHomeChild8} />
+      <div className={styles.protection}>Protection</div>
+      <div className={styles.dataProtectionGuarentees}>
+        Data protection guarentees
+      </div>
+      <div className={styles.flexibility}>Flexibility</div>
+      <div className={styles.customMadeFor}>
+        Custom made for your requirements
+      </div>
+      <div className={styles.easiness}>Easiness</div>
+      <div className={styles.goToMarketIn}>
+        Go-to market in less than 7 days
+      </div>
+      <div className={styles.tracking}>Tracking</div>
+      <div className={styles.trackAllYour}>
+        Track all your users’ financial transactions
+      </div>
+      <div className={styles.getStarted}>Get Started</div>
+      <div className={styles.learnMore}>Learn More</div>
+      <img className={styles.image2Icon} alt="" src="/image-2@2x.png" />
+      <img
+        className={styles.activitySvgrepoCom1Icon}
+        alt=""
+        src="/activitysvgrepocom-1.svg"
+      />
+      <img
+        className={styles.stickSvgrepoCom1Icon}
+        alt=""
+        src="/sticksvgrepocom-1.svg"
+      />
+      <img
+        className={styles.fireMinimalisticSvgrepoComIcon}
+        alt=""
+        src="/fireminimalisticsvgrepocom-1.svg"
+      />
+      <div className={styles.gatewayInvestpeHomeChild9} />
+      <div className={styles.gatewayInvestpeHomeChild10} />
+      <div className={styles.tryInvestpeNow}>Try Investpe Now</div>
+      <div className={styles.somePartnersWho}>
+        Some partners who make InvestPe products possible—
+      </div>
+      <div className={styles.andAFew}>
+        ...and a few of the businesses building some incredible solutions with
+        InvestPe
+      </div>
+      <img className={styles.image3Icon} alt="" src="/image-3@2x.png" />
+      <img className={styles.image4Icon} alt="" src="/image-4@2x.png" />
+      <div className={styles.lineDiv} />
+      <div className={styles.untitled1} />
+      <img
+        className={styles.drawsPatternSvgrepoCom1Icon}
+        alt=""
+        src="/drawspatternsvgrepocom-1.svg"
+      />
+      <img
+        className={styles.drawsPatternSvgrepoCom2Icon}
+        alt=""
+        src="/drawspatternsvgrepocom-2.svg"
+      />
+      <img className={styles.groupIcon} alt="" src="/group.svg" />
+      <img className={styles.image12Icon} alt="" src="/image-12@2x.png" />
+      <img className={styles.image13Icon} alt="" src="/image-13@2x.png" />
+      <img className={styles.image14Icon} alt="" src="/image-14@2x.png" />
+      <img className={styles.image15Icon} alt="" src="/image-15@2x.png" />
+      <img className={styles.image16Icon} alt="" src="/image-16@2x.png" />
+      <img className={styles.image17Icon} alt="" src="/image-17@2x.png" />
+      <b className={styles.connectYourPlatform}>
+        Connect your platform finances now with InvestPe Gateway
+      </b>
+      <div
+        className={styles.weHelpBrokers}
+      >{`We help brokers, wealth managers, fund managers & other businesses / startups server their customers better with our suite of modern digital investment stack`}</div>
+      <div className={styles.gatewayInvestpeHomeChild11} />
+      <div className={styles.rectangleGroup}>
+        <div className={styles.frameChild2} />
+        <div className={styles.sebiCompliantKycProcessParent}>
+          <div className={styles.sebiCompliantKyc}>
+            SEBI compliant KYC process
+          </div>
+          <div className={styles.multiAmcEmpanelments}>
+            Multi-AMC empanelments
+          </div>
+          <div
+            className={styles.transactTrack}
+          >{`Transact & Track orders`}</div>
+          <div className={styles.advancedReporting}>Advanced reporting</div>
+          <img
+            className={styles.checkCircleIcon}
+            alt=""
+            src="/checkcircle.svg"
+          />
+          <img
+            className={styles.checkCircleIcon1}
+            alt=""
+            src="/checkcircle.svg"
+          />
+          <img
+            className={styles.checkCircleIcon2}
+            alt=""
+            src="/checkcircle.svg"
+          />
+          <img
+            className={styles.checkCircleIcon3}
+            alt=""
+            src="/checkcircle.svg"
+          />
+        </div>
+        <div className={styles.mutualFunds1}>MUTUAL FUNDS</div>
+        <b className={styles.getTheCompleteContainer}>
+          <p className={styles.stayAheadWith}>Get the complete MF Infra</p>
+          <p
+            className={styles.stayAheadWith}
+          >{`& focus on customer engagement`}</p>
+        </b>
+      </div>
+      <img className={styles.dashboardIcon} alt="" src="/dashboard@2x.png" />
+      <div className={styles.gatewayInvestpeHomeChild12} />
+      <div className={styles.weValueYourContainer}>
+        <p className={styles.weValueYourPreciousTime}>
+          <b>We value your precious time.</b>
+        </p>
+        <p className={styles.blankLine}>&nbsp;</p>
+        <p className={styles.blankLine}>
+          No regulatory licenses required as we have everything for you to get
+          started.
+        </p>
+      </div>
+      <img
+        className={styles.rectangleIcon}
+        alt=""
+        src="/rectangle-149093.svg"
+      />
+      <img className={styles.image153Icon} alt="" src="/image-153@2x.png" />
+      <div className={styles.investpe}>INVESTPE</div>
+      <b className={styles.whyDoesIt}>
+        Why does it make sense to start with us?
+      </b>
+      <div className={styles.indianEquity}>INDIAN EQUITY</div>
+      <b className={styles.oneGatewayFor}>One gateway for all top securities</b>
+      <div className={styles.rectangleContainer}>
+        <div className={styles.frameChild3} />
+        <div className={styles.fixedDeposits}>FIXED DEPOSITS</div>
+        <b className={styles.fullyDigitalFds}>
+          Fully digital FDs for your users
+        </b>
+        <div className={styles.offerFixedDeposits}>
+          Offer fixed deposits on your app or website, from multiple banks.
+          Embed a pre-built SDK and go live in weeks.
+        </div>
+        <img
+          className={styles.ac60dcf7d1b43529fc657d6b94Icon}
+          alt=""
+          src="/0ac60dcf7d1b43529fc657d6b94e8af4-1.svg"
+        />
+        <img
+          className={styles.screenshot20230820At452}
+          alt=""
+          src="/screenshot-20230820-at-452-1@2x.png"
+        />
+      </div>
+      <div
+        className={styles.offerExpertCurated}
+      >{`Offer expert curated & monitored stock basket solutions`}</div>
+      <div className={styles.integratesWithAll}>
+        Integrates with all major brokers
+      </div>
+      <div
+        className={styles.enableStocksEtfs}
+      >{`Enable stocks, ETFs & expert curated stock baskets in your app`}</div>
+      <img className={styles.checkCircleIcon4} alt="" src="/checkcircle.svg" />
+      <img className={styles.checkCircleIcon5} alt="" src="/checkcircle.svg" />
+      <img className={styles.checkCircleIcon6} alt="" src="/checkcircle1.svg" />
+      <div className={styles.frameDiv}>
+        <div className={styles.frameChild4} />
+        <div className={styles.frameChild5} />
+        <div className={styles.submit}>Submit</div>
+        <b className={styles.howWouldYou}>
+          How would you like to work with us?
+        </b>
+        <div className={styles.frameChild6} />
+        <div className={styles.abcxyzcom}>abc@xyz.com</div>
+        <div className={styles.workEmail}>Work email</div>
+        <div className={styles.whichProductsAre}>
+          Which products are you interested in?
+        </div>
+        <div className={styles.frameChild7} />
+        <div className={styles.frameChild8} />
+        <div className={styles.frameChild9} />
+        <div className={styles.frameChild10} />
+        <div className={styles.frameChild11} />
+        <div className={styles.companyName}>Company Name</div>
+        <div className={styles.frameChild12} />
+        <div className={styles.mobileNumber}>Mobile Number</div>
+        <div className={styles.tellUsMore}>
+          Tell us more about yourself, and we’ll reach out to you as soon as
+          possible.
+        </div>
+        <img
+          className={styles.ac60dcf7d1b43529fc657d6b94Icon1}
+          alt=""
+          src="/0ac60dcf7d1b43529fc657d6b94e8af4-11.svg"
+        />
+        <img
+          className={styles.ec6a7d5Af154f4c9837299a2c1Icon}
+          alt=""
+          src="/2ec6a7d5af154f4c9837299a2c1511c2-1-1.svg"
+        />
+        <img
+          className={styles.b702430320041faAac59631b27Icon}
+          alt=""
+          src="/0b702430320041faaac59631b2745061-1.svg"
+        />
+        <img
+          className={styles.eae62d81e1a41fbB21f53cc428Icon}
+          alt=""
+          src="/5eae62d81e1a41fbb21f53cc428d7362-1.svg"
+        />
+      </div>
+      <div className={styles.mutualFunds2}>Mutual Funds</div>
+      <div className={styles.e2eInfrastructureFor}>
+        E2E infrastructure for mutual fund investing
+      </div>
+      <div className={styles.indianEquity1}>Indian Equity</div>
+      <div className={styles.stockGatewayContainer}>
+        <p className={styles.stayAheadWith}>{`Stock gateway &`}</p>
+        <p className={styles.stayAheadWith}>stock baskets</p>
+      </div>
+      <div className={styles.digitalGold1}>Digital Gold</div>
+      <div
+        className={styles.buySell}
+      >{`Buy & Sell digital gold hassle free`}</div>
+      <div className={styles.fixedDeposits1}>Fixed Deposits</div>
+      <div className={styles.bookFdsInContainer}>
+        <p
+          className={styles.stayAheadWith}
+        >{`Book FDs in 1click w/o opening `}</p>
+        <p className={styles.stayAheadWith}>a bank ac</p>
+      </div>
+      <div className={styles.groupDiv}>
+        <div className={styles.groupChild} />
+        <b className={styles.contactUs1}>Contact Us</b>
+        <div className={styles.div}>8089225625</div>
+        <div className={styles.contactchhotastockcom}>
+          contact@chhotastock.com
+        </div>
+        <b className={styles.company}>Company</b>
+        <b className={styles.quickLinks}>Quick Links</b>
+        <div className={styles.aboutUsPrivacyContainer}>
+          <span className={styles.aboutUsPrivacyContainer1}>
+            <p className={styles.stayAheadWith}>About Us</p>
+            <p className={styles.stayAheadWith}>Privacy policy</p>
+            <p className={styles.stayAheadWith}>Tems and Conditions</p>
+          </span>
+        </div>
+        <div className={styles.homeGatewayBlogContainer}>
+          <span className={styles.aboutUsPrivacyContainer1}>
+            <p className={styles.stayAheadWith}>Home</p>
+            <p className={styles.stayAheadWith}>Gateway</p>
+            <p className={styles.stayAheadWith}>Blog</p>
+          </span>
+        </div>
+        <img
+          className={styles.pxInstagramIcon2}
+          alt=""
+          src="/1200pxinstagram-icon-2@2x.png"
+        />
+        <img
+          className={styles.facebookLogoIconFileFacebo}
+          alt=""
+          src="/facebooklogoiconfilefacebookiconsvgwikimediacommons4-2@2x.png"
+        />
+        <img
+          className={styles.youtubeLogoIcone2}
+          alt=""
+          src="/youtubelogoicone-2@2x.png"
+        />
+        <img
+          className={styles.twitterLogoVectorPngCliparIcon}
+          alt=""
+          src="/twitterlogovectorpngclipart1-4@2x.png"
+        />
+        <b className={styles.findUsOn}>Find Us On</b>
+        <div className={styles.chhotastockTechnologiesPrivaContainer}>
+          <span className={styles.aboutUsPrivacyContainer1}>
+            <p className={styles.stayAheadWith}>
+              © 2022 Chhotastock Technologies Private Limited. All rights
+              reserved. CIN - U74999WB2012PTC184187 Chhotastock Technologies
+              Private Limited.
+            </p>
+            <p className={styles.stayAheadWith}>&nbsp;</p>
+            <p
+              className={styles.stayAheadWith}
+            >{`Chhotastock Technologies builds platforms & investment products to invest better in Indian investment asset classes. `}</p>
+            <p className={styles.stayAheadWith}>
+              Investing in Stocks/ETFs (Exchange Traded Funds) are subject to
+              market risks. Read all the related documents before investing.
+              Investors should consider all risk factors and consult their
+              financial advisor before investingRegistered Office: L151, 3rd
+              Floor, CK Pearl, 5th Main Road, HSR Layout Sector 6, Bengaluru,
+              Karnataka - 560102, India
+            </p>
+            <p className={styles.stayAheadWith}>&nbsp;</p>
+            <p className={styles.stayAheadWith}>
+              For any query / feedback / clarifications, email at
+              contact@chhotastock.com
+            </p>
+            <p className={styles.stayAheadWith}>&nbsp;</p>
+            <p
+              className={styles.stayAheadWith}
+            >{`All Investment ideas, model portfolios & advisory are built by SEBI Registered Investment advisors (RIAs) & Chhotastock only facilitates investments`}</p>
+            <p className={styles.stayAheadWith}>in such baskets, portfolios.</p>
+          </span>
+        </div>
+        <div className={styles.groupParent}>
+          <img className={styles.groupIcon1} alt="" src="/group1.svg" />
+          <b className={styles.downloadTheApp}>Download the App Now !</b>
+        </div>
+        <img
+          className={styles.investpe011211}
+          alt=""
+          src="/investpe-0112-1@2x.png"
+        />
+      </div>
+      <img className={styles.vectorIcon2} alt="" src="/vector2.svg" />
+    </div>
+  );
+};
+
+export default GatewayInvestPeHome;
